dd9a3c92fb995f1da646bc96806dce37b101e685
Nikhil Srinivas Regulapati <144737598+regulapati-n@users.noreply.github.com>
2024-02-19 17:36:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name                    = var.vpc_name
   auto_create_subnetworks = false
-  routing_mode            = 
+  routing_mode            = "REGION"
 }
 
 resource "google_compute_subnetwork" "webapp_subnet" {
@@ -17,7 +17,7 @@ resource "google_compute_subnetwork" "webapp_subnet" {
   region        = var.region
 }
 
-resource "google_compute_subnetwork" "db_subnet" {
+resource "google_compute_subnetwork "db_subnet" {
   name          = var.db_subnet_name
   network       = google_compute_network.vpc_network.self_link
   ip_cidr_range = var.db_subnet_cidr
